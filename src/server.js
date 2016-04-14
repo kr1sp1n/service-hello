2163f84c0bfac4e346f32b3f38e3fe4f538e93d6
Krispin Schulz <krispinone@googlemail.com>
2016-04-14 16:28:10 +0200

    add dynamic replacement of name in response

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 module.exports = function (config) {
   const http = require('http');
   const port = config.port;
+  const URL = require('url');
+  const qs = require('querystring');
   const greeter = require('./greeter.js')({
     defaultName: config.defaultName,
   });
 
   const server = http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end(greeter.sayHello());
+
+    // parse name out of the URL query part like /?name=Bob
+    const data = qs.parse(URL.parse(request.url).query);
+    response.end(greeter.sayHello(data.name));
   });
 
   server.start = function () {
