14a33e331a57180a9c0c908c1727306dc8242209
Krispin Schulz <krispinone@googlemail.com>
2016-04-14 09:11:31 +0200

    add docker-config

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var http = require('http');
+const http = require('http');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World!\n");
+const port = process.env.PORT || 8580;
+
+const server = http.createServer(function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello World!\n');
 });
 
-server.listen('0.0.0.0', 8580);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://0.0.0.0:8580/");
+server.listen(port, '0.0.0.0', function () {
+  console.log('Server running at %j', server.address());
+});
